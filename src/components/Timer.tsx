189909268948c8720d8bb05108d48c6e0799f5f5
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-20 17:25:36 +0530

    Start and stop the timers

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,25 +2,37 @@ import { useEffect, useRef, useState } from 'react';
 
 import Container from './UI/Container.tsx';
 import { type Timer as TimerProps } from '../store/timers-context.tsx';
+import { useTimersContext } from '../store/useTimersContext.ts';
 
 const Timer = function ({ name, duration }: TimerProps) {
   const interval = useRef<number | null>(null);
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
 
   if (remainingTime <= 0 && interval.current) {
     clearInterval(interval.current);
   }
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setRemainingTime((prevRemainingTime) => prevRemainingTime - 50);
-    }, 50);
+    let timer: number;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prevTime) => {
+          if (prevTime <= 0) {
+            return prevTime;
+          }
+          return prevTime - 50;
+        });
+      }, 50);
 
-    interval.current = timer;
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
 
     /* a cleanup function that's automatically called by React, right before this effect function runs again, if it ever runs again, or at least right before the component unmounts, so before the component is removed from the DOM, */
     return () => clearInterval(timer);
-  }, []);
+  }, [isRunning]);
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
