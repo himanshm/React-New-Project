2cf8386fff5b32b1721f48878774b60ab3db45ca
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-02-20 14:46:31 +0530

    Access context Type-Safe with a custom hook
    an exclaimation could also be used or we could use type-guards
    
    But that will have to be done every where the context will be used

--- src/store/timers-context.tsx
+++ src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, createContext } from 'react';
+import { type ReactNode, createContext, useContext } from 'react';
 
 type Timer = {
   name: string;
@@ -17,6 +17,17 @@ type TimersContextValue = TimersState & {
 };
 const TimersContext = createContext<TimersContextValue | null>(null);
 
+// Create a context Type-Safe custom hook
+export const useTimersContext = function () {
+  const timersCtx = useContext(TimersContext);
+
+  if (timersCtx === null) {
+    throw new Error('TimersContext is null - that should not be the case!');
+  }
+
+  return timersCtx;
+};
+
 type TimerContextProviderProps = {
   children: ReactNode;
 };
@@ -25,7 +36,7 @@ const TimersContextProvider = function ({
   children,
 }: TimerContextProviderProps) {
   const contextValue: TimersContextValue = {
-    isRunning: false,
+    isRunning: true,
     timers: [],
     addTimer: function (timerData: Timer) {
       // ...
@@ -43,3 +54,6 @@ const TimersContextProvider = function ({
     </TimersContext.Provider>
   );
 };
+
+// export { useTimersContext };
+export default TimersContextProvider;
